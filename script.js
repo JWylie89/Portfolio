a240035956bb26f34d48af2218c3302eeadbc1ad
JWylie89 <76948330+JWylie89@users.noreply.github.com>
2021-01-13 10:31:56 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Button for Project A (Alfie Bear Brew Co) - coming soon - website not ready
+function changeTextA() {
+    document.getElementById("comingsoonA").innerHTML = "Coming Soon in 2021";
+  }
+
+// Button for Project B (Sue Cooks) - coming soon - website not ready
+function changeTextB() {
+    document.getElementById("comingsoonB").innerHTML = "Coming Soon in 2021";
+  }
+
+// Button for Project C (JK Holiday Rental) - coming soon - website not ready
+function changeTextC() {
+    document.getElementById("comingsoonC").innerHTML = "Coming Soon in 2021";
+  }
